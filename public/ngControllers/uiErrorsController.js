fb9f8132fb0d603cb3478877307a1fdd6d8e3b14
Matt Lambert <herrlambert@gmail.com>
2016-03-09 09:14:53 -0800

    Continuing refactor to provide URL Group errors

--- public/ngControllers/uiErrorsController.js
+++ public/ngControllers/uiErrorsController.js
@@ -2,10 +2,11 @@
  * Created by mattlam on 3/7/2016.
  */
 angular.module('ninesWeb')
-.controller('uiErrorsCtrl', ['$scope', '$routeParams', 'Urls', 'UrlGroups', 'Errors',
-    function($scope, $routeParams, Urls, UrlGroups, Errors) {
+.controller('uiErrorsCtrl', ['$scope', '$routeParams', 'Urls', 'UrlGroups', 
+	'Errors', 'ErrorsByUrlGroup', function($scope, $routeParams, Urls, 
+	UrlGroups, Errors, ErrorsByUrlGroup) {
 
-        $scope.errors = Errors.query();                 // Errors model
+        $scope.errors = ErrorsByUrlGroup({ id: $routeParams.id });
         $scope.currentUrlGroupId = $routeParams.id;
 
     }
--- public/ngServices/errorsService.js
+++ public/ngServices/errorsService.js
@@ -5,4 +5,9 @@ angular.module('ninesWeb')
     .factory('Errors', ['$resource', function($resource) {
         return $resource('http://localhost:3000/errors/:id', null, {}
         );
+    }])
+    .factory('ErrorsByUrlGroup', ['$resource', function($resource) {
+    	return $resource('http://localhost:3000/errors/urlgroup/:id', 
+    		null, {}
+    	);
     }]);
