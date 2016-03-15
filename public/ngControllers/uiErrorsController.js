8ecc8a91cdc3bffcc1707e64ead8aac0aef2bf3d
Matt <mattlam@uw.edu>
2016-03-15 14:06:31 -0700

    Add commenting to Errors Controller

--- public/ngControllers/uiErrorsController.js
+++ public/ngControllers/uiErrorsController.js
@@ -1,28 +1,37 @@
 /**
  * Created by mattlam on 3/7/2016.
+ *
+ * Main controller for Error view
  */
 angular.module('ninesWeb')
 .controller('uiErrorsCtrl', ['$scope', '$routeParams', '$sce',
     'ErrorsByUrlGroup', 'SharedFuncs',
     function($scope, $routeParams, $sce, ErrorsByUrlGroup, SharedFuncs) {
 
+        // Retrieve all errors associated with the given URL Group ID
         $scope.errors = ErrorsByUrlGroup.query({ id: $routeParams.id });
+        // Indicate the error for which user is viewing details
         $scope.currentErrorId = null;
-        $scope.currentErrorResonseUrl = null;
 
-
+        // Retrieve HTML page for follow-up full request to instance of error
+        // on header request
         $scope.setCurrentError = function(error) {
+            // Show details only for error selected
             $scope.currentErrorId = error._id;
+            // Build URL to be used to get full request response HTML and
+            // make available to view
             var errorResponseUrl = "http://localhost:3000/errors/response/"
                 + error._id;
             $scope.currentErrorResponseUrl =
                 $sce.trustAsResourceUrl(errorResponseUrl);
         };
 
+        // Hide (close) the detail view for any/all errors
         $scope.clearCurrentError = function() {
             $scope.currentErrorId = null;
         };
 
+        // Provide to view the month, day, and year elements of error date-time
         $scope.getErrorDate = function(dateTime) {
             // Verify that a value was passed to dateTime parameter
             if (dateTime) {
@@ -40,6 +49,8 @@ angular.module('ninesWeb')
                 return "No date data available.";
             }
         };
+
+        // Provide to view the hour and minute elements of the error date-time
         $scope.getErrorTime = function(dateTime) {
             // Verify that a value was passed to dateTime parameter
             if (dateTime) {
